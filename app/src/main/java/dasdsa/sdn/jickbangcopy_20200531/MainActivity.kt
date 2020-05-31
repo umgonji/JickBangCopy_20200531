c7096a772bcba348fc4478376a3687487436ea8a
umgonji <umgonji@naver.com>
2020-05-31 11:31:11 +0900

    Initial commit

---
+++ app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/MainActivity.kt
@@ -0,0 +1,12 @@
+package dasdsa.sdn.jickbangcopy_20200531
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/datas/JickBang.kt
@@ -0,0 +1,12 @@
+package dasdsa.sdn.jickbangcopy_20200531.datas
+
+import java.io.Serializable
+
+class JickBang(price: Int, address: String, floor: Int, description: String) : Serializable{
+
+    val price = price
+    val address =address
+    val floor = floor
+    val description = description
+
+}
