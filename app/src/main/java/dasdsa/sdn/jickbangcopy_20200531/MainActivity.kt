c65763b6e8fc5a12256ea45d20a477ae4d2f8dd8
umgonji <umgonji@naver.com>
2020-05-31 12:12:38 +0900

    메인액티비티 - BaseActivity상속으로 변경  - BaseActivity 에 있는 setupEvents(), setValues() 도 메인에 추가해놔야 한다.

--- app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/MainActivity.kt
@@ -3,10 +3,21 @@ package dasdsa.sdn.jickbangcopy_20200531
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
 }
