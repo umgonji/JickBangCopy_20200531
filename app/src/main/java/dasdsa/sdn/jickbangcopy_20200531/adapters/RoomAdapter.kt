59ac748bb62bd64c9617acbd7fa2425b3483b981
umgonji <umgonji@naver.com>
2020-05-31 12:02:10 +0900

    어댑터 생성자 / 상속 처리 (RoomAdapter )

---
+++ app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/adapters/RoomAdapter.kt
@@ -0,0 +1,10 @@
+package dasdsa.sdn.jickbangcopy_20200531.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import dasdsa.sdn.jickbangcopy_20200531.datas.Room
+
+class RoomAdapter(context:Context, resId:Int, list:List<Room>) : ArrayAdapter<Room>(context, resId, list) {
+
+
+}
