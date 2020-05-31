8513f981e63a9d06e1b646898bc0b79c55bf076a
umgonji <umgonji@naver.com>
2020-05-31 13:42:32 +0900

    설명 / 층수를 표시  => 가격 표시 기능 필요

--- app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/adapters/RoomAdapter.kt
+++ app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import dasdsa.sdn.jickbangcopy_20200531.R
 import dasdsa.sdn.jickbangcopy_20200531.datas.Room
 
@@ -22,8 +23,15 @@ class RoomAdapter(context:Context, resId:Int, list:List<Room>) : ArrayAdapter<Ro
         }
         val row = tempRow!!
 
+        //출력 데이터 가져오기
+        val data = mList.get(position)
 
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descTxt = row.findViewById<TextView>(R.id.descTxt)
 
+        addressAndFloorTxt.text = "${data.address}, ${data.getFormatFloor()}"
+        descTxt.text = data.description
 
 
         return row
--- app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/datas/Room.kt
+++ app/src/main/java/dasdsa/sdn/jickbangcopy_20200531/datas/Room.kt
@@ -2,4 +2,17 @@ package dasdsa.sdn.jickbangcopy_20200531.datas
 
 class Room(val price:Int, val address:String, val floor: Int, val description:String) {
 
+    fun getFormatFloor() : String {
+
+        if(this.floor > 0) {
+            return "${this.floor}층"
+        }
+        else if (this.floor == 0) {
+            return "반지하"
+        }
+        else {
+            return "지하 ${-this.floor}층"
+        }
+
+    }
 }
